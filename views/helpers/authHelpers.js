8ebbcab7f5e160f397b926cce1fff83eb614be1f
Japhet Elias <japhet9@gmail.com>
2021-10-03 10:36:14 +0000

    refactored the authentication middleware i made to another file. used app.locals to access needed variabls

---
+++ views/helpers/authHelpers.js
@@ -0,0 +1,24 @@
+const { validateUser } = require('./userHelpers');
+
+module.exports = function () {  
+  return (req, res, next) => {
+    const whiteListRoutes = ['/', '/login', '/register'];
+    const path = req.path;
+
+    if (whiteListRoutes.includes(path)) {
+      if(validateUser(req.cookies['user_id'], req.app.locals.users)) { // if valid logged in user tries to request login/request/'root' page redirect to /urls page
+        return res.redirect('/urls');
+      }
+      res.clearCookie('user_id'); // clears old/invalid cookie
+      return next(); // else show login page
+    }
+    if (/^\/u\//.test(path)) { //a regex to check if req.path starts with /u/ (i.e. is a shortURL)
+      return next();
+    }
+    if (!validateUser(req.cookies['user_id'], req.app.locals.users)) { // when 1. user_id cookie doesn't exist 2. or user_id cookie is invalid (i.e. using an old/modified user_id) redirect all requests
+      res.clearCookie('user_id'); // clears old/invalid cookie
+      return res.redirect('/login');
+    }
+    return next();
+}
+};
